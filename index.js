6f71f0c73f62650f134749ede0af5860177ec0fe
doniprastyo25 <donismsbanking@gmail.com>
2022-04-26 13:45:52 +0700

    test

---
+++ index.js
@@ -0,0 +1,156 @@
+'use strict';
+const express = require('express');
+const axios = require('axios');
+const helmet = require('helmet');
+const open = require('open');
+const compression = require('compression');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const isOnline = require('is-online');
+const db = require('./src/data/database').db
+// const multer = require('multer');
+
+const appRoutes = require('./src/routes/appRoutes');
+const inRoutes = require('./src/routes/inRoutes');
+const outRoutes = require('./src/routes/outRoutes');
+
+const app = express();
+app.use(helmet());
+app.use(compression());
+
+// view engine setup
+app.set('views', path.join(__dirname, '/src/views'));
+app.set('view engine', 'ejs');
+
+//flash config
+app.use(cookieParser('secretMMSPObyrikiyukie2022'));
+app.use(
+    session({
+        cookie: {expires: 6000},
+        secret: 'secretMMSPObyrikiyukie2022',
+        resave: false,
+        saveUninitialized: false
+    })
+);
+app.use(flash());
+
+//layouts
+app.use(expressLayouts);
+app.use(express.urlencoded({ extended: true }));
+
+//route
+app.use("/", appRoutes.routes);
+//routes
+app.use("/penerimaan", inRoutes.inroutes);
+app.use("/pengeluaran", outRoutes.outroutes);
+
+//static
+app.use("/gambar", express.static(path.join(__dirname + "/img/logo")));
+app.use("/docs", express.static(path.join(__dirname, 'docs')));
+app.use("/styles", express.static(path.join(__dirname + "/src/assets/bootstrap")));
+app.use("/fonts", express.static(path.join(__dirname + "/src/assets/fonts")));
+app.use("/icon", express.static(path.join(__dirname + "/src/assets/bootstrap-icons")));
+app.use("/ressources", express.static(path.join(__dirname + "/src/assets/ressources")));
+app.use("/filepath", express.static(path.join(__dirname + "/upload")));
+app.use("node", express.static(path.join(__dirname + "/node_modules/read-excel-file/node")));
+
+//server
+app.listen(8000, ()=>{
+    console.log("");
+    console.log(" =====================================");
+    console.log("                (SAKUMU)              ");
+    console.log(" Sistem Aplikasi Keuangan Muhammadiyah");
+    console.log(" =====================================");
+    console.log("");
+    console.log(" Powered by :");
+    console.log(" PT BPRS Mitra Mentari Sejahtera");
+    console.log("");
+    console.log(" url : http://localhost:8000");
+    console.log("");
+    console.log(" app version 1.1");
+    console.log(" _________________________________");
+    console.log("");
+    
+    // openlink();
+
+});
+
+
+// -------------------------------service cek koneksi-----------------------------------------//
+setInterval(() => {
+    (async () => {
+        // console.log(await isOnline());
+        if (await isOnline() == true) {
+            console.log('sedang online');
+            gettoken(function(data) {
+                // console.log(data.status);
+                if (data.status == true) {
+                    // console.log(data.token);
+                    try {
+                        axios({
+                            method: 'get',
+                            // url: 'http://localhost:3123/API/cektoken',
+                            url: 'http://467a0269edbd.sn.mynetname.net:80/API/cektoken',
+                            // url: 'http://192.168.151.31:3123/API/cektoken',
+                            data: {token: data.token},
+                            timeout: 10000
+                        }).then(function(response) {
+                            const res = response.data;
+                            if (res.data.status == 'ok') {
+                                console.log('token sesuai');
+                            } else {
+                                console.log('token tidak sesuai');
+                                try {
+                                    db.prepare(`DELETE FROM APPCONFIG`).run();
+                                } catch (error) {
+                                    console.log('request time ouut');
+                                }
+                            }
+                        }).catch(function (error) {
+                            if (error.code == 'ECONNREFUSED') {
+                                console.log('server sedang offline');
+                            }
+                        })
+                    } catch (error) {
+                        // console.log(error);
+                        console.log('request  time out');
+                    }
+                } else {
+                    console.log('error');
+                }
+            })
+        } else {
+            console.log('sedang offline');
+        }
+    })();
+  }, 100000);
+
+  const gettoken = async (callback) => {
+    try {
+        const get = `SELECT * FROM APPCONFIG`;
+        const tok = db.prepare(get).get();
+        if (tok) {
+            const token = tok.token
+            callback({status:true , token: token})
+            // console.log(token);
+        } else {
+            // callback({status:false , token: error})
+            console.log('token belum ada');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+//----------------------------------- end service cek koneksi --------------------------------------//
+
+
+async function openlink() {
+    await open('http://localhost:8000', {app: {name: 'chrome'}});
+}
+module.exports = {
+    app,
+    openlink
+};
---
+++ src/data/kas.js
@@ -0,0 +1,154 @@
+'use strict';
+const db = require('./database').db
+
+const getSaldoKas = async function(callback) {
+    let query = "SELECT * FROM KAS";
+    let getkas = [];
+
+    try {
+        const rows = db.prepare(query).all();
+        for (let i = 0; i < rows.length; i++) {
+            const idkas = rows[i].id;
+            getkas.push("(SELECT SUM(total) as in"+idkas+" FROM PENERIMAAN WHERE kas='"+idkas+"'),(SELECT SUM(total) as out"+idkas+" FROM PENGELUARAN WHERE kas='"+idkas+"')")
+        }
+
+        const getinkas = "SELECT * FROM "+getkas.toString();
+        try {
+            const inkas = db.prepare(getinkas).get();
+            const datajs = JSON.stringify(inkas, function(key, value) {
+                return (value === null) ? 0 : value;
+            });
+
+            const finaldata = {
+                kas:rows,
+                inout:JSON.parse(datajs)
+            }
+            //console.log(finaldata);
+            callback(finaldata);
+        } catch (error) {
+            console.log(error);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getSumberDana = async function(callback) {
+    await getKas(function(data) {
+        const query = "SELECT *FROM SUMBERDANA order by id";
+        try {
+            const rows = db.prepare(query).all();
+            callback({status:"ok",kas: data,sumberdana:rows});
+        } catch (error) {
+            console.log(error);
+        }
+    });
+}
+
+const addSumberDana = async function(sd,callback) {
+    let nomor = 1;
+    let getno = "SELECT max(id) as no FROM SUMBERDANA";
+    try {
+        const nomax = db.prepare(getno).get();
+        if (nomax.no != null) {
+            nomor = nomax.no + 1
+        }
+        let insert = "INSERT INTO SUMBERDANA (id,nama) VALUES ("+nomor+",'"+sd+"')";
+        try {
+            const pre = db.prepare(insert);
+            const run = pre.run();
+            if (run.changes > 0) {
+                callback({status: "ok", msg: "sukses"});
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const delSumberDana = async function(id,nama,callback) {
+    const query = "DELETE FROM SUMBERDANA WHERE id="+id+" AND nama='"+nama+"'";
+    try {
+        const run = db.prepare(query).run();
+        if (run.changes > 0) {
+            callback({status: "ok",msg: "- Sumber dana "+nama+" berhasil dihapus."});
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getKas = async function(callback) {
+    const query = "SELECT *FROM KAS"
+    try {
+        const exe = db.prepare(query).all();
+        return callback(exe);
+    } catch (error) {
+        console.log();
+    }
+}
+
+const addKas = async function(nama_kas,color, callback) {
+    await getKas(function(data) {
+        const id = data.length+1
+        const query = "INSERT INTO KAS (id,nama,color) VALUES ('kas"+id+"','"+nama_kas+"','"+color+"')";
+        try {
+            const exe = db.prepare(query).run();
+            if (exe.changes > 0) {
+                return callback({status: "ok", msg: "kas berhasil ditambahkan"});
+            }
+        } catch (error) {
+            console.log();
+        }
+    });
+}
+
+const delKas = async function (id,callback) {
+  const kd1 = "PENERIMAAN";
+  const kd2 = "PENGELUARAN";
+  let que1 = `DELETE FROM KAS WHERE id=@id`;
+  // hapus semua data penerimaan dan pengeluaran berdasarkan kas
+  let que2 = `DELETE FROM ${kd1} WHERE kas=@id AND EXISTS(SELECT *FROM ${kd1} WHERE Kas=@id LIMIT 1)`;
+  let que3 = `DELETE FROM ${kd2} WHERE kas=@id AND EXISTS(SELECT *FROM ${kd2} WHERE Kas=@id LIMIT 1)`;
+
+  try {
+      const statement = [que1,que2, que3].map(sql => db.prepare(sql));
+      const transaction = db.transaction((data)=>{
+          for(const stmt of statement){
+            stmt.run(data);        
+          }
+      });
+      transaction({id:id});
+      // console.log(transaction);
+
+      callback({status:'ok', msg:'sukses'});
+      console.log('- Menu berhasil dihapus');
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+const editKas = async function (id, nama, callback) {
+  let update = `UPDATE KAS SET nama= '${nama}' WHERE id= '${id}'`
+  console.log(update);
+  try {
+    const pre = db.prepare(update).run();
+    if (pre.changes > 0) {
+      return callback({status:"ok", msg:"sukses"})
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+    getSaldoKas,
+    getSumberDana,
+    addSumberDana,
+    delSumberDana,
+    addKas,
+    editKas,
+    delKas
+}
